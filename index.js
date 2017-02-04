4a5ce4a4f196073451e4fbfaa446ddc1534b909e
Tiger Oakes <tigeroakes@gmail.com>
2017-02-04 14:27:24 -0800

    Added 'chrome' env that turns off certain presets

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 module.exports = {
   presets: [
-    require('babel-preset-es2017'),
-    require('babel-preset-es2016'),
     require('babel-preset-react'),
   ],
   plugins: [
     require('babel-plugin-external-helpers'),
   ],
+  env: {
+    development: {
+      presets: [
+        require('babel-preset-es2017'),
+        require('babel-preset-es2016'),
+      ],
+    },
+    chrome: {},
+  },
 };
